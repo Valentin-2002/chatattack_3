c3d6c449e1e834900c59c2d52daafe0c6fdd38a3
Valentin-2002 <vwetts@gmail.com>
2020-10-29 16:34:04 +0100

    Removed unused Code

--- db-connection.php
+++ db-connection.php
@@ -25,23 +25,6 @@ try{
 
 }
 
-function fetch_user_last_activity($user_id, $pdo)
-{
-	$query = "
-	SELECT * FROM login_details 
-	WHERE user_id = '$user_id' 
-	ORDER BY last_activity DESC 
-	LIMIT 1
-	";
-	$statement = $pdo->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-	foreach($result as $row)
-	{
-		return $row['last_activity'];
-	}
-}
-
 function fetch_user_chat_history($from_user_id, $to_user_id, $pdo)
 {
 	$query = "
